d95952f625b310eb182115be20a1c7ed0960c1b1
chatomud <chatomud@gmail.com>
2018-04-01 09:01:51 +0200

    sanitize user input to UTF-8

--- lib/cm_socket.rb
+++ lib/cm_socket.rb
@@ -4,6 +4,7 @@ module ChatoMud
 
     def rx
       input = gets
+      input = input.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
       return [:bad, nil] unless input
       return [:ok, input.strip!]
     end
